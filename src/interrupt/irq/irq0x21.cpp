921e53221d2a2a694b8aaa205e55a38fe2da5b5b
fishjump <z1052525203@outlook.com>
2021-04-06 13:17:47 +0800

    remove debug info

--- src/interrupt/irq/irq0x21.cpp
+++ src/interrupt/irq/irq0x21.cpp
@@ -5,10 +5,6 @@
 #include "../irq.hpp"
 
 void system::interrupt::irq::irq0x21(interrupt_frame *frame) {
-    // system::io::TextModeScreen &tm =
-    // system::io::TextModeScreen::getGobalInstance(); system::io::Keyboard
-    // &keyboard = system::io::Keyboard::getGobalInstance();
-
     system::io::entity::TextScreen *text_screen =
         system::global::instance::getTextScreen();
 
@@ -18,6 +14,5 @@ void system::interrupt::irq::irq0x21(interrupt_frame *frame) {
     uint8_t scan_code = system::io::port::readBtye(0x60);
 
     keyboard->enqueue(scan_code);
-    text_screen->print((uint64_t)scan_code)->print("\n");
     system::io::port::writeBtye(0x20, 0x20);
 }
